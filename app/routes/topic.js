709f88b0a3b83b7a60730526f62fadeccc4ed34a
Lauren Posey <mrslaurenposey@gmail.com>
2016-08-29 11:29:02 -0700

    allow you to delete an individual topic

--- app/routes/topic.js
+++ app/routes/topic.js
@@ -3,6 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('topic', params.topic_id);
-  }
+  },
 
+  actions: {
+    destroyTopic(topic) {
+      topic.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
 });
