b4576cdacf3938f7b3dc6bf15ca373f4afa8e5b5
Lauren Posey <mrslaurenposey@gmail.com>
2016-08-29 11:18:20 -0700

    allow you to edit the topics in the homepage

--- app/routes/index.js
+++ app/routes/index.js
@@ -10,6 +10,15 @@ export default Ember.Route.extend({
       var newTopic = this.store.createRecord('topic', params);
       newTopic.save();
       this.transitionTo('index');
+    },
+    edit(topic, params) {
+      Object.keys(params).forEach(function(key) {
+        if (params[key] !== undefined) {
+        topic.set(key,params[key]);
+        }
+      });
+      topic.save();
+      this.transitionTo('index');
     }
   }
 });
